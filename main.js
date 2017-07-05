c27e7854b406d935c4c8e604d5a420bb57466e90
rickmurdock <rick.murdock@live.com>
2017-07-05 15:34:04 -0500

    Converted routes to use mongodb.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 const port = 3001;
 const app = express();
+const dbUrl = "mongodb://localhost:27017/robotdb";
+
+let DB;
+let ROBOTDBROBOTS;
 
 var userData = require('./data.js');
 
@@ -11,14 +17,35 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-  res.render('index', {userBox: userData});
-})
+// CONNECT to MongoDB
+mongoClient.connect(dbUrl, function(err, db) {
+  if(err) {
+    console.warn("Error connecting to database", err);
+  }
+  
+  DB = db;
+  ROBOTDBROBOTS = db.collection("robots");
+});
 
-app.get('/:id', function (req, res) {
-  var i = req.params.id - 1;
-  res.render('detail', {userBox: userData.users[i]});
-})
+app.get("/", (req, res) => {
+  ROBOTDBROBOTS.find({}).toArray(function(err, foundRobots) {
+    if(err) {
+      console.warn("Error finding robots robotdb", err);
+    }
+    
+   res.render("index", {robots: foundRobots});
+  });
+});
+
+app.get("/:id", (req, res)=>{
+  ROBOTDBROBOTS.findOne({ _id: ObjectId(req.params.id) }, function(err, foundRobot) {
+      if(err){
+        res.status(500).send(err);
+      }
+
+      res.render("detail", {robot: foundRobot});
+  });
+});
 
 app.listen(port, function() {
   console.log('Express user directory Started on port', port);
